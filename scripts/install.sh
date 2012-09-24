3fa59112166e667aa23abd99a93de1a02142565e
Richard Butler <rich@aspectvision.com>
2012-09-24 23:01:46 +0100

    Added installer for jscoverage.

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Add /usr/local/src
+[ ! -d /usr/local/src ] && sudo mkdir /usr/local/src
+
 # Install coffee script
 
 if ! which coffee >/dev/null; then
@@ -18,15 +21,23 @@ if ! which supervisor >/dev/null; then
 	sudo npm install -g supervisor
 fi
 
+# Install jscoverage
+
+if ! which jscoverage >/dev/null; then
+	cd /usr/local/src
+	sudo wget http://siliconforks.com/jscoverage/download/jscoverage-0.5.1.tar.bz2
+	sudo tar -xvf jscoverage-0.5.1.tar.bz2
+	cd jscoverage-0.5.1
+	sudo ./configure
+	sudo make && sudo make install
+fi
+
 # Install phantom
 
 # No longer required for the moment
 exit 0
 
 if ! which phantomjs >/dev/null; then
-
-	# Add /usr/local/src
-	[ ! -d /usr/local/src ] && sudo mkdir /usr/local/src
 
 	cd /usr/local/src
 	sudo git clone git://github.com/ariya/phantomjs.git
